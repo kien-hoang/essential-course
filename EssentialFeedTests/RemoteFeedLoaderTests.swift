1fe5053a26efff3c1d6613664d14bce905a6a14a
kien-hoang <htkien.dev@gmail.com>
2023-02-04 19:23:36 +0700

    `RemoveFeedLoader` does not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,25 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Bradley Hoang on 03/02/2023.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
