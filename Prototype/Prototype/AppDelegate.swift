f59146c9e0c96389bc02ceb2be505f7a2692c78c
kien-hoang <htkien.dev@gmail.com>
2023-03-11 21:47:16 +0700

    Restruct project. Combine EssentialFeed and Prototype into one project

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Bradley Hoang on 10/03/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
